ccea701fdb19fc376293a4a707a0e9f6c039903a
dnakano <dnakano@student.42tokyo.jp>
2021-08-28 14:34:31 +0900

    add add todo component

--- openapi-todo/client-react/src/App.tsx
+++ openapi-todo/client-react/src/App.tsx
@@ -1,58 +1,41 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Todo from "./pages/Todo";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-        <Router>
+    <Router>
       <div>
         <nav>
+          <h1>Sample App</h1>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
+              <Link to="/todo">Todo</Link>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/todo">
+            <Todo />
+          </Route>
+          <Route>
+            <NotFound />
           </Route>
         </Switch>
       </div>
     </Router>
   );
-}
-
-function About() {
-  return <p>About</p>
-}
-
-function Users() {
-  return <p>Users</p>
-}
-
-function Home() {
-  return <p>Home</p>
 }
 
 export default App;
