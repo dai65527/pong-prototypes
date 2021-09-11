5b51c8e1b0884320ec3589a51264b1f542c62e5c
dnakano <dnakano@student.42tokyo.jp>
2021-09-11 23:43:42 +0900

    add server as youtube

---
+++ chatsample/server/README.md
@@ -0,0 +1,9 @@
+## startup
+```
+docker-compose up -d
+```
+
+## connect db
+```
+psql -h 127.0.0.1 -U chatdbuser -d yt_message_db
+```
---
+++ chatsample/server/src/message/message.gateway.ts
@@ -0,0 +1,55 @@
+import { Inject, Logger } from "@nestjs/common";
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from "@nestjs/websockets";
+import { Server } from "http";
+import { MessageService } from "./message.service";
+
+@WebSocketGateway(4000, { namespace: "message", cors: true })
+export class MessageGateway
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+  @Inject()
+  private messageService: MessageService;
+
+  @WebSocketServer()
+  private wss: Server;
+
+  private logger = new Logger("MessageGateway");
+  private count = 0;
+
+  async handleConnection(client: any, ...args: any[]) {
+    this.count += 1;
+    this.logger.log(`Connected: ${this.count} connections`);
+    const messages = await this.messageService.getAll();
+    client.emit("all-messages-to-client", messages);
+  }
+
+  public async handleDisconnect(client: any) {
+    this.count += 1;
+    this.logger.log(`Disconnected: ${this.count} connections`);
+    const messages = await this.messageService.getAll();
+    client.emit("all-messages-to-client", messages);
+  }
+
+  afterInit(server: any) {
+    this.logger.log("MessageGateway Initialized");
+  }
+
+  @SubscribeMessage("new-message-to-server")
+  public async handleNewMessage(
+    @MessageBody() data: { sender: string; message: string },
+  ): Promise<void> {
+    const message = await this.messageService.createMessage(
+      data.sender,
+      data.message,
+    );
+    this.wss.emit("new-message-to-client", { message });
+  }
+}
