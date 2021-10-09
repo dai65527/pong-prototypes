41b6086128af52cae773fe5d35b46013a5bcd031
dnakano <dnakano@student.42tokyo.jp>
2021-10-09 18:39:58 +0900

    auth small sample done!

--- authSmallSample/app.js
+++ authSmallSample/app.js
@@ -1,30 +1,79 @@
 var express = require("express");
 var passport = require("passport");
 var bodyParser = require("body-parser");
-var multer = require('multer');
+var multer = require("multer");
+var jwt = require("jsonwebtoken");
+var cookieParser = require("cookie-parser");
+var fetch = require("node-fetch");
 
 var app = express();
 var upload = multer();
-app.use(upload.array()); 
+app.use(upload.array());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(passport.initialize());
+app.use(cookieParser());
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 var server = app.listen(3000, function () {
   console.log("listening on port " + server.address().port);
 });
 
-// insecure request
-app.get("/api/insecure", function (req, res) {
-  res.send("Insecure response.\n");
-});
+// JWT Strategy
+var JwtStrategy = require("passport-jwt").Strategy;
+var ExtractJwt = require("passport-jwt").ExtractJwt;
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        function (req) {
+          if (req && req.cookies) {
+            return req.cookies["jwt"];
+          }
+          return null;
+        },
+      ]),
+      secretOrKey: "thisissecretvalue",
+    },
+    function (jwtPayload, done) {
+      return done(null, jwtPayload);
+    }
+  )
+);
+
+// info request
+app.get(
+  "/api/info",
+  passport.authenticate("jwt", { sesion: false }),
+  function (req, res) {
+    console.log(`safe access from ${req.user.id}`)
+    res.send(`Hello ${req.user.id}!!\nThis is secret information`);
+  }
+);
+
+// set jwt token
+function setJwtCookie(res, id) {
+  var token = jwt.sign({ id }, "thisissecretvalue", { expiresIn: "1h" });
+  res.cookie("jwt", token, {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: true,
+  });
+}
 
 // secure request with local strategy
 var LocalStrategy = require("passport-local").Strategy;
 passport.use(
   new LocalStrategy(function (username, password, done) {
-    console.log("local strategy")
-    if (username === "admin" && password === "admin") {
+    console.log("access to local strategy");
+    if (username === "user" && password === "pass") {
       return done(null, username);
     } else {
       return done(null, false);
@@ -32,35 +81,54 @@ passport.use(
   })
 );
 
-app.post('/api/secure/local', passport.authenticate('local', { session: false }), function(req, res){
-  res.send("Secure response from " + JSON.stringify(req.body.username));
-});
+app.post(
+  "/api/login/local",
+  passport.authenticate("local", {
+    session: false,
+    failureRedirect: "/login",
+  }),
+  function (req, res) {
+    setJwtCookie(res, req.user);
+    return res.redirect("/");
+  }
+);
 
 // secure request with OAuth2 strategy (42api)
 var OAuth2Strategy = require("passport-oauth2").Strategy;
-passport.use("ftapi", new OAuth2Strategy({
-    authorizationURL: "https://api.intra.42.fr/oauth/authorize",
-    tokenURL: "https://api.intra.42.fr/oauth/token",
-    clientID: process.env.FTAPI_UID,
-    clientSecret: process.env.FTAPI_SECRET,
-    callbackURL: "http://localhost:3000/api/secre/ftapi/callback",
-    // callbackURL: "https://42tokyo.jp/",
-    // callbackURL: "https://example.com",
-  }, function (accessToken, refreshToken, profile, done) {
-    console.log("oauth2 strategy");
-    return done(null, profile);
+passport.use(
+  "ftapi",
+  new OAuth2Strategy(
+    {
+      authorizationURL: "https://api.intra.42.fr/oauth/authorize",
+      tokenURL: "https://api.intra.42.fr/oauth/token",
+      clientID: process.env.FTAPI_UID,
+      clientSecret: process.env.FTAPI_SECRET,
+      callbackURL: "http://localhost:3000/api/login/ftapi/callback",
+      scope: ["public"]
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      console.log("access to oauth2 strategy");
+      var res = await fetch("https://api.intra.42.fr/v2/me", {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      })
+      var body = await res.json();
+      return done(null, body.login);
+    }
+  )
+);
+
+app.get(
+  "/api/login/ftapi",
+  passport.authenticate("ftapi", { session: false, failureRedirect: "/login" }),
+);
+
+app.get(
+  "/api/login/ftapi/callback",
+  passport.authenticate("ftapi", { session: false, failureRedirect: "/login" }),
+  async function (req, res) {
+    setJwtCookie(res, req.user);
+    return res.redirect("/");
   }
-));
-
-app.get('/api/secure/ftapi/callback', passport.authenticate('ftapi', { session: false }), function(req, res) {
-  console.log(req.body);
-  console.log(req.body.username);
-  res.send("Secure response from " + JSON.stringify(req.body.username));
-  redirect('http://localhost:3000');
-});
-
-app.get('/api/secure/ftapi', passport.authenticate('ftapi', { session: false }), function(req, res) {
-  console.log(req.body);
-  console.log(req.body.username);
-  res.send("Secure response from " + JSON.stringify(req.body.username));
-});
+);
