0c7eedbdc23e23364299ab3f969b490f789c4feb
dnakano <dnakano@student.42tokyo.jp>
2021-10-12 22:34:22 +0900

    create react app

--- chatsample/server/src/app.module.ts
+++ chatsample/server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ServeStaticModule } from "@nestjs/serve-static";
 import { MessageModule } from "./message/message.module";
 
 @Module({
