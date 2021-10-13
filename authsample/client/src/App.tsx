4124f21557b50e1e163275184850a5a6f16270b1
dnakano <dnakano@student.42tokyo.jp>
2021-10-13 09:10:22 +0900

    set up react router

--- authsample/client/src/App.tsx
+++ authsample/client/src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+            <p>This is about page.</p>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+            <p>This is users page.</p>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+            <p>This is home page.</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
