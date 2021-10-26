170595beb0ff36e039a3b2308dad9d0757f702e0
hiroin <egamescape@gmail.com>
2021-10-26 20:04:00 +0900

    CSRF追加

--- two-factor-auth/src/app.controller.ts
+++ two-factor-auth/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import csurf from 'csurf';
 import { AppService } from './app.service';
+import { Csrf } from 'ncsrf';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/token')
+  getCsrfToken(@Req() req, @Res() res): any {
+    res.json({ csrfToken: req.csrfToken() });
+    return 'Set Token';
+  }
+
+  @Post()
+  needProtect(): string {
+    return 'Protected!';
   }
 }
--- two-factor-auth/src/main.ts
+++ two-factor-auth/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { Logger } from '@nestjs/common';
+import * as csurf from 'csurf';
+import { nestCsrf, CsrfFilter } from 'ncsrf';
 
 async function bootstrap() {
   const logger: Logger = new Logger('main.ts');
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  // app.use(csurf);
+  app.use(csurf({ cookie: true }));
+  // app.use(nestCsrf());
   await app.listen(process.env.SERVER_PORT, () =>
     logger.log(`Server started on port ${process.env.SERVER_PORT}`),
   );
